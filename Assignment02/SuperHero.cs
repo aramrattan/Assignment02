da39c1aa8749813331f9c1cc2ab9298d675762de
aramrattan <arlina.ramrattan@gmail.com>
2015-02-04 14:28:22 -0500

    Made show method

--- Assignment02/SuperHero.cs
+++ Assignment02/SuperHero.cs
@@ -11,21 +11,32 @@ namespace Assignment02
         //*****************Private Properties*************************
         private string[] superPowers = {"Super Speed", "Super Strength","Body Armour", "Flight", "Fire Generation","Weather Control"};
         private Random rnd = new Random();
- 
+        private string[] herosPowers = new string[3];
 
         //*****************Constructor Method*************************
         public SuperHero(string name): base (name)
         {
             Hero sHero = new Hero("The Avatar");
+            this.name = name;
             generateRandomPowers();
         }
 
         //*****************Private Methods****************************
-        private string[] generateRandomPowers()
+        private void generateRandomPowers()
         {
-            string[] herosPowers = new string[3];
+           
 
+             
+        }
 
+        //****************Public Methods******************************
+        public void show()
+        {
+            Console.WriteLine("{0}'s powers are:");
+            Console.WriteLine("-{0}", herosPowers[0]);
+            Console.WriteLine("-{0}", herosPowers[1]);
+            Console.WriteLine("-{0}", herosPowers[2]);
+   
         }
 
     }
