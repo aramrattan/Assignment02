c65fbb6b67809ba197982960d7e2cf95eb6ee96a
aramrattan <arlina.ramrattan@gmail.com>
2015-02-04 09:56:10 -0500

    Private String array and constructor method

--- Assignment02/SuperHero.cs
+++ Assignment02/SuperHero.cs
@@ -8,6 +8,15 @@ namespace Assignment02
 {
     class SuperHero : Hero 
     {
+        //*****************Private Properties*************************
+        private string[] superPowers;
+ 
 
+        //*****************Constructor Method*************************
+        public SuperHero(string name): base (name)
+        {
+            Hero sHero = new Hero("Athena");
+            generateRandomPowers();
+        }
     }
 }
